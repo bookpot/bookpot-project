74262de83e17723d8417a88e2d9b4c335a4c73a8
yujung7768903 <yu-jung31476@naver.com>
2021-03-11 09:56:00 -0800

    #9-Home JS / js파일 생성+로그인 버튼 누르면 흐려지기

---
+++ Home JS.js
@@ -0,0 +1,8 @@
+'use strict'
+const body = document.getElementsByTagName("body");
+
+$(document).ready(function(){
+    $("body").click(function(){
+        $("body").toggleClass("login-tab");
+    })
+})

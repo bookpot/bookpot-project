384f251187fb46e1f9a9ad699549398742e560f2
yujung7768903 <yu-jung31476@naver.com>
2021-03-12 21:49:19 -0800

    9-Home-JS/JS 시작

--- Home JS.js
+++ Home JS.js
@@ -1,8 +1,19 @@
 'use strict'
 const body = document.getElementsByTagName("body");
+const loginForm = document.getElementsByClassName("login-form");
 
 $(document).ready(function(){
     $("body").click(function(){
         $("body").toggleClass("login-tab");
     })
 })
+
+function loginTab() {
+    if (loginForm.style.display == "none") {
+        loginForm.style.display = "block";
+        console.log("none->block");
+    } else {
+        loginForm.style.display = "none";
+        console.log("block->none");
+    }
+}

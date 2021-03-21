16982bd3158ae9984f40e97eb3329ac0c556c61d
yujung7768903 <yu-jung31476@naver.com>
2021-03-22 03:07:35 +0900

    9-Home-JS/view 전환 성공

--- Home JS.js
+++ Home JS.js
@@ -2,6 +2,16 @@
 const body = document.getElementsByTagName("body");
 const loginForm = document.getElementsByClassName("login-form");
 const loginFormContainer = document.getElementById("login-form-container");
+let gridContentSummary = document.getElementsByClassName("summary");
+let bookTypeField = document.getElementsByClassName("field");
+let viewType = document.getElementsByClassName("view-type");
+let viewTypeSelect = document.getElementsByClassName("view-type-select");
+let listViewSelected = document.getElementById("list-view-selected");
+let gridViewSelected = document.getElementById("grid-view-selected");
+let listView = document.getElementById("list-view");
+let gridView = document.getElementById("grid-view");
+let indexOfView
+//view 인덱스 변수 선언하기
 
 $(document).ready(function(){
     $(".login").click(function(){
@@ -16,3 +26,14 @@ $(document).ready(function(){
         $(".login-form").hide();
     })
 })
+
+function contentSummaryPrint() {
+    for (let i=0; i<gridContentSummary.length; i++) {
+        console.log(gridContentSummary[i]);
+        gridContentSummary[i].innerText = gridContentSummary[i].innerText.slice(0,46) + "...";
+    }
+}
+contentSummaryPrint();
+
+listViewSelected.addEventListener("click", function(){listView.style.display = "flex"; gridView.style.display = "none"});
+gridViewSelected.addEventListener("click", function(){listView.style.display = "none"; gridView.style.display = "flex"});

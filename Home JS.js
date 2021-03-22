317056b40a83d4bf8e9c3717181d50a697494aba
yujung7768903 <yu-jung31476@naver.com>
2021-03-23 03:35:17 +0900

    9-Home-JS/grid 뷰 제목 시작점 맞춤, 리스트 뷰 항목 수정(분야 뺌)

--- Home JS.js
+++ Home JS.js
@@ -11,8 +11,8 @@ let gridViewSelected = document.getElementById("grid-view-selected");
 let listView = document.getElementById("list-view");
 let gridView = document.getElementById("grid-view");
 let indexOfView
-//view 인덱스 변수 선언하기
 
+// 로그인 창 띄우고&닫기
 $(document).ready(function(){
     $(".login").click(function(){
         $("body").toggleClass("login-form-show");
@@ -27,13 +27,30 @@ $(document).ready(function(){
     })
 })
 
+//분야 선택
+function bookTypeSelected(iOfField) {
+    console.log(bookTypeField[iOfField].style.backgroundColor);
+    if (bookTypeField[iOfField].style.backgroundColor == "#FFFFFF") {
+        console.log("하이라이트로 변경");
+        bookTypeField[iOfField].style.backgroundColor = "#4FBA80";
+        bookTypeField[iOfField].style.color = "#FFFFFF";
+    }
+    else{
+        console.log("기본으로 변경");
+        bookTypeField[iOfField].style.backgroundColor = "#FFFFFF";
+        bookTypeField[iOfField].style.color = "#000000";
+    }
+}
+
+// grid view content의 내용 요약
 function contentSummaryPrint() {
     for (let i=0; i<gridContentSummary.length; i++) {
-        console.log(gridContentSummary[i]);
+        console.log(gridContentSummary[i].innerText);
         gridContentSummary[i].innerText = gridContentSummary[i].innerText.slice(0,46) + "...";
     }
 }
 contentSummaryPrint();
 
-listViewSelected.addEventListener("click", function(){listView.style.display = "flex"; gridView.style.display = "none"});
-gridViewSelected.addEventListener("click", function(){listView.style.display = "none"; gridView.style.display = "flex"});
+// view 전환
+listViewSelected.addEventListener("click", function(){listView.style.display = "flex"; gridView.style.display = "none" });
+gridViewSelected.addEventListener("click", function(){listView.style.display = "none"; gridView.style.display = "flex" });
